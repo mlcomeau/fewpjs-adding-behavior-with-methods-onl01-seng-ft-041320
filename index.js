0524d03ab808a4ee565a8b586afc7961e330e357
Megan Comeau <mlcomeau99@gmail.com>
2020-07-27 21:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ class Cat {
   speak() {
     return `${this.name} says meow!`;
   }
+};
+
+class Dog {
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
 }
