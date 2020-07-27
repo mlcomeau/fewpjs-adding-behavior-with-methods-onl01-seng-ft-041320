aed4cb8eb441151cc46d8c696a9d228319842d90
Megan Comeau <mlcomeau99@gmail.com>
2020-07-27 21:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ class Bird {
 
   speak() {
     if (this.gender === 'male') {
-      return `It\'s me! ${this.name}, the parrot!`
+      return `It\'s me! ${this.name}, the parrot!`;
     }
+    else {
+      return `${this.name} says squawk!`;
+    };
   }
 };
